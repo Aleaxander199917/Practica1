54d0b6f4ba1856bf31b855bf0f3d05ae4254ce2f
Aleaxander199917 <sascha.beloded@gmail.com>
2021-04-19 15:03:23 +0300

    Edit Stuff Class!

--- Practica1/Company.cs
+++ Practica1/Company.cs
@@ -9,5 +9,65 @@ namespace Practica1
         private string Title;
         private string Address;
         private int YearOfFoundation;
+        public Company()
+        {
+            Title = "";
+            Address = "";
+            YearOfFoundation = 0;
+        }
+        public void ShowTitle()
+        {
+
+            Console.WriteLine("The current company title is {0}", Title);
+        }
+        public void ShowAddress()
+        {
+
+            Console.WriteLine("The current company address is {0}", Address);
+        }
+        public void ShowYearOfFoundation()
+        {
+
+            Console.WriteLine("The current company year of foundation = {0}", YearOfFoundation);
+
+        }
+        public string TITLE
+        {
+            get
+
+            {
+                return Title;
+            }
+            set
+            {
+                Title = value;
+            }
+
+        }
+        public string ADDRESS
+        {
+            get
+
+            {
+                return Address;
+            }
+            set
+            {
+                Address = value;
+            }
+
+        }
+        public int YEAROFFOUNDATION
+        {
+            get
+            {
+
+                return YearOfFoundation;
+            }
+            set
+            {
+                YearOfFoundation = value;
+            }
+        }
     }
 }
